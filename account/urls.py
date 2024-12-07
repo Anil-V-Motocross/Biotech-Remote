0f2688a3b12f0da557eaed239a4d814467aace49
Rahul1744 <58468972+Rahul1744@users.noreply.github.com>
2024-12-07 16:28:51 +0530

    v11

--- account/urls.py
+++ account/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path
-# from .views.register import register
-from .views.login import login
-from .views.role import create_group_and_assign_permissions
+from .views.register import register_staff
+from .views.login import login_staff
+from .views.role import create_group_and_assign_permissions, get_permissions
 from .views.register_mobile import register_mobile, validate_otp, register
 
 urlpatterns = [
     path('registerWithMobile/', register_mobile, name='register_mobile'),
     path('validateOtp/', validate_otp, name='validate_otp'),
     path('register/', register , name='register'),
-    path('login/', login, name='login'),
+    path('register-staff/', register_staff, name='register_staff'),
+    path('login-staff/', login_staff, name='login_staff'),
     # path('logout/', views.logout, name='logout'),
 
     path('create-group/', create_group_and_assign_permissions, name='create_group_and_assign_permissions'),
+    path('get-permissions/', get_permissions, name='get_permissions')
 ]
