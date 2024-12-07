7c094de6f78c7b48885035a134e7e6ab8b244170
Rahul1744 <58468972+Rahul1744@users.noreply.github.com>
2024-12-07 23:19:22 +0530

    v20

--- attribute/view/color.py
+++ attribute/view/color.py
@@ -14,7 +14,7 @@ class ColorSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST', 'PATCH', 'DELETE'])
 @permission_classes([IsAuthenticated, DynamicPermission])
 @authentication_classes([JWTAuthentication])
 def color(request):
@@ -33,3 +33,53 @@ def color(request):
             'colors': serializer.data
         }
         return Response(data={'message': 'success', 'data': data}, status=status.HTTP_200_OK)
+    
+    if request.method == 'POST':
+        required_permissions = [
+            'attribute.add_color'
+        ]
+        
+        if not any(request.user.has_perm(perm) for perm in required_permissions):
+            return Response({'message': 'You do not have permission to perform this action.'}, status=status.HTTP_403_FORBIDDEN)
+        
+        serializer = ColorSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data={'message': 'success'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    if request.method == 'PATCH':
+        required_permissions = [
+            'attribute.change_color'
+        ]
+        
+        if not any(request.user.has_perm(perm) for perm in required_permissions):
+            return Response({'message': 'You do not have permission to perform this action.'}, status=status.HTTP_403_FORBIDDEN)
+        
+        color_id = request.data.get('color_id', None)
+
+        if not color_id:
+            return Response({'message': 'color_id is required.'}, status=status.HTTP_400_BAD_REQUEST)
+        color = Color.objects.get(id=color_id)
+        serializer = ColorSerializer(color, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data={'message': 'success'}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    if request.method == 'DELETE':
+        required_permissions = [
+            'attribute.delete_color'
+        ]
+        
+        if not any(request.user.has_perm(perm) for perm in required_permissions):
+            return Response({'message': 'You do not have permission to perform this action.'}, status=status.HTTP_403_FORBIDDEN)
+        
+        color_id = request.data.get('color_id', None)
+
+        if not color_id:
+            return Response({'message': 'color_id is required.'}, status=status.HTTP_400_BAD_REQUEST)
+        if Color.objects.filter(id=color_id).exists():
+            Color.objects.filter(id=color_id).delete()
+            return Response(data={'message': 'success'}, status=status.HTTP_200_OK)
+        return Response(data={'message': 'color not found'}, status=status.HTTP_404_NOT_FOUND)
