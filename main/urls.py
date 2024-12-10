557befbb79a415bce1be63befa6bc871ba8cfefa
Rahul1744 <58468972+Rahul1744@users.noreply.github.com>
2024-12-10 23:48:57 +0530

    v41

--- main/urls.py
+++ main/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -34,6 +36,9 @@ urlpatterns = [
     path('services/',include('services.urls')),
     path('coupon/',include('coupon.urls')),
     path('blog/',include('blog.urls')),
+
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
 
 if settings.DEBUG:
