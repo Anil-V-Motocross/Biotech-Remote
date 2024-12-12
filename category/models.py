24eeb5091181356e3c40a28d29e2329f97f4a8d0
Rahul1744 <58468972+Rahul1744@users.noreply.github.com>
2024-12-12 20:08:49 +0530

    v49

--- category/models.py
+++ category/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='category/', default='default/category_default.png')
     is_published = models.BooleanField(default=True)
 
     def __str__(self):
