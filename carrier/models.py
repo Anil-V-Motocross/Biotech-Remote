4c5d683bf9c5380c6970394dd811f888cdf5996b
Rahul1744 <58468972+Rahul1744@users.noreply.github.com>
2024-12-15 16:17:55 +0530

    v60

--- carrier/models.py
+++ carrier/models.py
@@ -2,7 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Carrier(models.Model):
-    categories = models.CharField(max_length=100)
+    CATEGORY_CHOICES = [
+        ('Tech Positions', 'Tech Positions'),
+        ('Non-Tech Positions', 'Non-Tech Positions'),
+    ]
+
+    categories = models.CharField(
+        max_length=30,
+        choices=CATEGORY_CHOICES,
+        default='Non-Tech Positions'
+    )
     position_name = models.CharField(max_length=100)
     job_summary = models.TextField()
     responsibilities = models.TextField()
