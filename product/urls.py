4e48ddf031c8269f4d114434ba28ec430be0e590
Rahul1744 <58468972+Rahul1744@users.noreply.github.com>
2024-12-15 23:50:43 +0530

    v66

--- product/urls.py
+++ product/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .view.product import product
+from .view.add_category_subcategory_tags import add_category_subcategory_tags
 
 app_name = 'product'
 
 urlpatterns = [
     path('', product, name='product'),
+    path('addCategorySubcategoryTags/', add_category_subcategory_tags, name='add_category_subcategory_tags'),
 ]
