2329c618b47dd9aa86d53faf5f75b0103f0c828d
Rahul1744 <58468972+Rahul1744@users.noreply.github.com>
2024-12-25 19:41:05 +0530

    v87

--- product/view/default_product.py
+++ product/view/default_product.py
@@ -4,25 +4,40 @@ from rest_framework import status
 from product.models import Product
 from rest_framework import serializers
 from product.models import Product, MainProductImage
+from django.conf import settings
 
 class ProductSerializer(serializers.ModelSerializer):
     # Include main product image as the first element in the images list
     images = serializers.SerializerMethodField()
+    # add short_description from MainProduct
+    short_description = serializers.ReadOnlyField(source='product_id.short_description')
+    main_product_name = serializers.ReadOnlyField(source='product_id.name')
 
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = ['id', 'name', 'price', 'is_default', 'images', 'short_description', 'main_product_name']
 
     def get_images(self, obj):
         # Start with the product's main image
-        image_list = [{"image": obj.image.url}] if obj.image else []
+        image_list = [{"image": self.get_absolute_url(obj.image)}] if obj.image else []
         
         # Add the related MainProductImages
         main_product_images = MainProductImage.objects.filter(product=obj.product_id)
         for main_product_image in main_product_images:
-            image_list.append({"image": main_product_image.image.url})
+            image_list.append({"image": self.get_absolute_url(main_product_image.image)})
 
         return image_list
+
+    def get_absolute_url(self, image_field):
+        """
+        This method constructs the absolute URL for the given image field.
+        It uses request.build_absolute_uri() with MEDIA_URL to construct the full URL.
+        """
+        request = self.context.get('request')  # Get the request object from the context
+        if request and image_field:
+            # Generate the full absolute URL for the image
+            return request.build_absolute_uri(settings.MEDIA_URL + image_field.name)
+        return None 
 
 
 @api_view(['GET'])
@@ -36,7 +51,7 @@ def default_product(request, product_id=None):
             return Response(data={'message': 'Product not found.'}, status=status.HTTP_404_NOT_FOUND)
         
         # Serialize the single product (do not use `many=True`)
-        serializer = ProductSerializer(product)
+        serializer = ProductSerializer(product, context={'request': request})
         
         # Prepare the response data
         data = {
