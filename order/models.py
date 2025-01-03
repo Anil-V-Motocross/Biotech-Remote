36667d96b34f5483363c2660aa9a59580569d376
Rahul1744 <58468972+Rahul1744@users.noreply.github.com>
2025-01-03 19:29:29 +0530

    v105

--- order/models.py
+++ order/models.py
@@ -5,28 +5,28 @@ from account.models import User
 # Create your models here.
 
 class Order(models.Model):
-    order_id = models.CharField(max_length=50)
+    order_id = models.CharField(max_length=50, default=0)
     date = models.DateField()
     
     customer_id = models.ForeignKey(User, on_delete=models.CASCADE)
     customer_name = models.CharField(max_length=50)
-    grand_total = models.CharField(max_length=10, default=0)
+    grand_total = models.FloatField(default=0)
     email = models.EmailField()
     mobile = models.CharField(max_length=15)
-    address = models.CharField(max_length=200)
+    address = models.CharField(max_length=200, null=True, blank=True)
     tracking_id = models.CharField(max_length=50)
     payment_method = models.CharField(max_length=50)
     status = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.order_id
+        return str(self.id)
     
 class OrderItem(models.Model):
     order_id = models.ForeignKey(Order, on_delete=models.CASCADE)
     product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.CharField(max_length=10)
-    price = models.CharField(max_length=10)
-    total = models.CharField(max_length=10)
+    quantity = models.IntegerField()
+    price = models.FloatField()
+    total = models.FloatField()
     
 
     def __str__(self):
