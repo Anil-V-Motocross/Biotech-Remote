ceaa7bae17fc0b3a47e8c76fd003558c40082ce4
Rahul1744 <58468972+Rahul1744@users.noreply.github.com>
2025-01-05 13:09:48 +0530

    v106

--- account/urls.py
+++ account/urls.py
@@ -3,6 +3,7 @@ from .views.register import register_staff
 from .views.login import login_staff
 from .views.role import create_group_and_assign_permissions, get_permissions
 from .views.register_mobile import register_mobile, validate_otp, register
+from .views.all_users import all_users
 
 name = 'account'
 
@@ -12,7 +13,7 @@ urlpatterns = [
     path('register/', register , name='register'),
     path('register-staff/', register_staff, name='register_staff'),
     path('login-staff/', login_staff, name='login_staff'),
-    # path('logout/', views.logout, name='logout'),
+    path('all_users/', all_users, name='all_users'),
 
     path('create-group/', create_group_and_assign_permissions, name='create_group_and_assign_permissions'),
     path('get-permissions/', get_permissions, name='get_permissions')
