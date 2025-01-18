a35d5f296dad81a74bf96c17fcba240e72c4bba0
Rahul1744 <58468972+Rahul1744@users.noreply.github.com>
2025-01-18 11:05:56 +0530

    v124

--- services/urls.py
+++ services/urls.py
@@ -1,11 +1,13 @@
-from .views import create_service_enquiry,get_service_enquiries
-from .views import servicelist
+from .views import create_service_enquiry, get_service_enquiries, publicservicelist, servicelist
+
 from django.urls import path
 
 urlpatterns = [
     path('service_enquiry/',create_service_enquiry, name='create_service_enquiry'),
     path('get_service_enquiries/',get_service_enquiries, name='get_service_enquiries'),
+    path('get_service_enquiries/<int:pk>/',get_service_enquiries,name='get_service_enquiries'),
 
-    path('service_list/',servicelist, name='servicelist'),
-
+    path('publicservice_list/',publicservicelist, name='publicservicelist'),
+    path('servicelist/', servicelist, name='servicelist'),  # For list and create operations
+    path('servicelist/<int:pk>/', servicelist, name='servicelist-detail'),
 ]
